22a893aa46d02069d36cbed354fc40f0f7e5304d
Jisay <jean-christophe.alleman@corp.ovh.com>
2021-06-15 15:33:53 +0200

    test: add test to fit v1.0.0
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

---
+++ packages/reket-core/tests/config/request-types.spec.js
@@ -0,0 +1,47 @@
+import { ReketConfigRequestTypes } from '../../src/config/request-types';
+
+describe('ReketConfigRequestTypes implementation', () => {
+  test('it should instanciate a ReketConfigRequestTypes instance', () => {
+    const requestTypesConfig = new ReketConfigRequestTypes();
+
+    expect(requestTypesConfig.size).toBe(0);
+    expect(requestTypesConfig.getDefaultUrlPrefix()).toBe(undefined);
+  });
+});
+
+describe('add method', () => {
+  test('it should add a request type with its value', () => {
+    const requestTypesConfig = new ReketConfigRequestTypes();
+
+    requestTypesConfig.add('myType', '/my/type');
+
+    expect(requestTypesConfig.size).toBe(1);
+    expect(requestTypesConfig.getUrlPrefix('myType')).toBe('/my/type');
+    expect(requestTypesConfig.getDefaultUrlPrefix()).toBe('/my/type');
+  });
+
+  test('it should add mutiple request types with their values', () => {
+    const requestTypesConfig = new ReketConfigRequestTypes();
+
+    requestTypesConfig.add([
+      {
+        type: 'myFirstType',
+        urlPrefix: '/my/first/type',
+      },
+      {
+        type: 'mySecondType',
+        urlPrefix: '/my/second/type',
+      },
+      {
+        type: 'myThirdType',
+        urlPrefix: '/my/third/type',
+      },
+    ]);
+
+    expect(requestTypesConfig.size).toBe(3);
+    expect(requestTypesConfig.getUrlPrefix('myThirdType')).toBe(
+      '/my/third/type',
+    );
+    expect(requestTypesConfig.getDefaultUrlPrefix()).toBe('/my/first/type');
+  });
+});
---
+++ packages/reket-core/tests/hook.spec.js
@@ -0,0 +1,62 @@
+import { ReketHook } from '../src/hook';
+
+describe('ReketHook implementation', () => {
+  test('it should instanciate a ReketHook instance', () => {
+    const reketHook = new ReketHook();
+
+    expect(reketHook.onSuccess).toBe(undefined);
+    expect(reketHook.onError).toBe(undefined);
+  });
+});
+
+describe('set method', () => {
+  test('it should define onSuccess function', () => {
+    const reketHook = new ReketHook();
+
+    reketHook.set(() => {});
+
+    expect(reketHook.onSuccess).not.toBe(undefined);
+    expect(typeof reketHook.onSuccess === 'function').toBe(true);
+    expect(reketHook.onError).toBe(undefined);
+  });
+
+  test('it should define onError function', () => {
+    const reketHook = new ReketHook();
+
+    reketHook.set(undefined, () => {});
+
+    expect(reketHook.onSuccess).toBe(undefined);
+    expect(reketHook.onError).not.toBe(undefined);
+    expect(typeof reketHook.onError === 'function').toBe(true);
+  });
+
+  test('it should define onSuccess and onError functions', () => {
+    const reketHook = new ReketHook();
+
+    reketHook.set(
+      () => {},
+      () => {},
+    );
+
+    expect(reketHook.onSuccess).not.toBe(undefined);
+    expect(typeof reketHook.onSuccess === 'function').toBe(true);
+    expect(reketHook.onError).not.toBe(undefined);
+    expect(typeof reketHook.onError === 'function').toBe(true);
+  });
+
+  test('it should throw an error if onSuccess if not a function', () => {
+    const reketHook = new ReketHook();
+
+    expect(() => {
+      reketHook.set('not a function', () => {});
+    }).toThrow(Error);
+  });
+
+  test('it should throw an error if onError if not a function', () => {
+    const reketHook = new ReketHook();
+
+    expect(() => {
+      reketHook.set(() => {}, 'not a function');
+    }).toThrow(Error);
+  });
+});
